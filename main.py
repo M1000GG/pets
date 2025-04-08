0c748bb13a09036a326aabc6a74df242c9319ade
Mateo Gutiérrez Delgado <mgutierrez107696@umanizales.edu.co>
2025-04-08 18:28:23 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from controller.pet_controller import router as pet_router
-
-app = FastAPI()
-app.include_router(pet_router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
